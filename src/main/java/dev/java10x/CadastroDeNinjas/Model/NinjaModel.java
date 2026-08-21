f1af26a21291dcf647277f1480b3f674999d6e85
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-08-20 21:32:07 -0300

    create: criado uma entidade no banco de dados a partir do NinjaModel [aula03]

--- src/main/java/dev/java10x/CadastroDeNinjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Model/NinjaModel.java
@@ -1,4 +1,49 @@
 package dev.java10x.CadastroDeNinjas.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro") // nomeando a tabela do Database
 public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+
+    public NinjaModel(){}
+
+    public NinjaModel(String name, Integer age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters e Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
