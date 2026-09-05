dd244491a39b27d45e951e17e8df12118159930d
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-09-05 17:14:35 -0300

    fix: Inclui a anotation @JsonIgnore para previnir o erro de serialização

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissaoModel.java
@@ -0,0 +1,35 @@
+package dev.java10x.CadastroDeNinjas.Missoes.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.CadastroDeNinjas.Ninjas.Model.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class MissaoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    /*
+    * @OneToMany -> Uma missão para muitos Ninjas
+    *     Foreign key -> mappadBy = "nome_da_coluna_relacionada"
+    */
+    @OneToMany(mappedBy = "missoes")
+    @JsonIgnore // Essa anotação ignora a serialização que ocorre e gera um loop infinito
+    private List<NinjaModel> ninjas;
+}
